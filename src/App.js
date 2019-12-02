67b6b523af35c6178fe1d48286eb9bb6524a9ba2
R4tch3t <bebetovic@hotmail.com>
2019-12-02 13:07:46 -0600

    change load scripts

--- src/App.js
+++ src/App.js
@@ -17,9 +17,7 @@ export default class App extends React.Component {
             return (
               <>
               <Helmet>
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+                
               </Helmet>
               <BrowserRouter>
                 <div>
